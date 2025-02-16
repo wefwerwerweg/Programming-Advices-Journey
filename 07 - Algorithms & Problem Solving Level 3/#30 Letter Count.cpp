cad16e562c2cc41c4a0bcb7c06e0d2a12dc77e57
Mohamed Osama <102436778+M-0S@users.noreply.github.com>
2025-02-16 19:35:48 +0200

    Create #30 Letter Count.cpp

---
+++ 07 - Algorithms & Problem Solving Level 3/#30 Letter Count.cpp
@@ -0,0 +1,27 @@
+// #30 Letter Count
+// https://programmingadvices.com/courses/1869980/lectures/42658373/comments/25844120
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int LetterCount(string text, char c)
+{
+	short count = 0;
+	for (short i = 0; i < text.length(); i++)
+		if (text[i] == c) count++;
+	return count;
+}
+
+int main()
+{
+	string text;
+	char c;
+	cout << "Please enter a string:\n";
+	getline(cin, text);
+	cout << "\nPlease enter a character:\n";
+	cin >> c;
+	cout << "\nLetter '" << c << "' count = " << LetterCount(text, c) << endl;
+	return 0;
+}
