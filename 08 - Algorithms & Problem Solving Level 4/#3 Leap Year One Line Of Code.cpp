887f93d0010d0ba206331db4c51ca5b373cc6c30
Mohamed Osama <102436778+M-0S@users.noreply.github.com>
2025-02-19 05:34:03 +0200

    Create #3 Leap Year One Line Of Code.cpp

---
+++ 08 - Algorithms & Problem Solving Level 4/#3 Leap Year One Line Of Code.cpp
@@ -0,0 +1,23 @@
+// #3 Leap Year One Line Of Code
+// https://programmingadvices.com/courses/1890122/lectures/42994250/comments/25862436
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+bool IsLeapYear(int year)
+{
+	return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+}
+
+int main()
+{
+	int year;
+	cout << "Enter a year: ";
+	cin >> year;
+	if (IsLeapYear(year))
+		cout << "Yes, it is a leap year.\n";
+	else cout << "No, it is not a leap year.\n";
+	return 0;
+}
