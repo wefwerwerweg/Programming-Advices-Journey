7ccade22fbb7911d94af72164a824798d727abdd
Mohamed Osama <102436778+M-0S@users.noreply.github.com>
2025-02-19 22:08:44 +0200

    Create #16 Increase Date By 1 Day.cpp

---
+++ 08 - Algorithms & Problem Solving Level 4/#16 Increase Date By 1 Day.cpp
@@ -0,0 +1,114 @@
+// #16 Increase Date By 1 Day
+// https://programmingadvices.com/courses/1890122/lectures/43070793/comments/25868433
+
+#include <iostream>
+
+using namespace std;
+
+struct stDate
+{
+	short year;
+	short month;
+	short day;
+};
+
+bool IsLeapYear(short year)
+{
+	return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+}
+
+short GetMonthDays(short year, short month)
+{
+	short MonthsDays[12] = { 31,28,31,30,31,30,31,31,30,31,30,31 };
+	return month == 2 ? (IsLeapYear(year) ? 29 : 28) : MonthsDays[month - 1];
+}
+
+bool IsLastDayInMonth(stDate date)
+{
+	return date.day == GetMonthDays(date.year, date.month);
+}
+
+bool IsLastMonthInYear(short month)
+{
+	return month == 12;
+}
+
+stDate IncreaseDateByOne(stDate date)
+{
+	if (IsLastDayInMonth(date))
+	{
+		if (IsLastMonthInYear(date.month))
+		{
+			date.day = 1;
+			date.month = 1;
+			date.year++;
+		}
+		else
+		{
+			date.day = 1;
+			date.month++;
+		}
+	}
+	else date.day++;
+
+	return date;
+}
+
+short ShortInput()
+{
+	short input;
+	cin >> input;
+	while (cin.fail() || input < 1)
+	{
+		cin.clear();
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+		cout << "Invalid input! Enter a valid one: ";
+		cin >> input;
+	}
+	return input;
+}
+
+short MonthInput()
+{
+	short input = ShortInput();
+	while (input > 12)
+	{
+		cout << "Invalid input! Enter a valid one: ";
+		input = ShortInput();
+	}
+	return input;
+}
+
+
+short DayInput(short year, short month)
+{
+	short input = ShortInput();
+	while (input > GetMonthDays(year, month))
+	{
+		cout << "Invalid input! Enter a valid one: ";
+		input = ShortInput();
+	}
+	return input;
+}
+
+stDate ReadDate()
+{
+	stDate date;
+	cout << "Enter a year : ";
+	date.year = ShortInput();
+	cout << "Enter a month: ";
+	date.month = MonthInput();
+	cout << "Enter a day  : ";
+	date.day = DayInput(date.year, date.month);
+	return date;
+}
+
+int main()
+{
+	stDate date = ReadDate();
+	date = IncreaseDateByOne(date);
+	printf("\nDate after adding one day: ");
+	printf("%02d/%02d/%d", date.day, date.month, date.year);
+	printf("\n");
+	return 0;
+}
