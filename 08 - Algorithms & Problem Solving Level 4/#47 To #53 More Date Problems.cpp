dc7c2047bc4a668dbd801ca68e096bd93a387827
Mohamed Osama <102436778+M-0S@users.noreply.github.com>
2025-02-21 14:00:12 +0200

    Update #47 To #53 More Date Problems.cpp

--- 08 - Algorithms & Problem Solving Level 4/#47 To #53 More Date Problems.cpp
+++ 08 - Algorithms & Problem Solving Level 4/#47 To #53 More Date Problems.cpp
@@ -1,8 +1,6 @@
 // #47 To #53 More Date Problems
 // https://programmingadvices.com/courses/1890122/lectures/43136394/comments/25881337
 
-#pragma warning(disable : 4996)
-
 #include <iostream>
 
 using namespace std;
