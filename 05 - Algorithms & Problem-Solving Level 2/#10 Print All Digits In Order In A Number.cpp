86548c7a4b1abdef09cb49e5fcd8e0c43e7170d2
wefwerwerweg <162762660+wefwerwerweg@users.noreply.github.com>
2025-03-07 08:46:29 +0300

    Update #10 Print All Digits In Order In A Number.cpp

--- 05 - Algorithms & Problem-Solving Level 2/#10 Print All Digits In Order In A Number.cpp
+++ 05 - Algorithms & Problem-Solving Level 2/#10 Print All Digits In Order In A Number.cpp
@@ -1,20 +1,42 @@
 // Print All Digits In Order In A Number
-// https://programmingadvices.com/courses/1811531/lectures/41327515/comments/25773346
+// Coded By: @X99099
 
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-void PrintInOrder(string n)
-{
-	for (int i = 0; i < n.length(); i++)
-		cout << n[i] << endl;
+int getPositiveNumber(const string& prompt) {
+    int num;
+    do {
+        cout << prompt << endl;
+        cin >> num;
+    } while (num <= 0);
+    
+    return num;
 }
 
-int main()
-{
-	string n;
-	cout << "Print Numbers In Order In: ";
-	cin >> n;
-	PrintInOrder(n);
+int reverseNumber(int num) {
+    int remainder = 0, reversed = 0;
+    
+    while (num > 0) {
+        remainder = num % 10;
+        num /= 10;
+        reversed = reversed * 10 + remainder;
+    }
+    
+    return reversed;
+}
+
+void printDigits(int num) {
+    while (num > 0) {
+        cout << num % 10 << endl;
+        num /= 10;
+    }
+}
+
+int main() {
+    int num = getPositiveNumber("Enter a positive number:");
+    printDigits(reverseNumber(num));
+    
+    return 0;
 }
