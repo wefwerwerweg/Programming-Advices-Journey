f36562ae92f7c56313094c5aea6aec979dc51d83
wefwerwerweg <162762660+wefwerwerweg@users.noreply.github.com>
2025-03-07 07:09:57 +0300

    Update #36 Add Elements To Array Semi Dynamic.cpp

--- 05 - Algorithms & Problem-Solving Level 2/#36 Add Elements To Array Semi Dynamic.cpp
+++ 05 - Algorithms & Problem-Solving Level 2/#36 Add Elements To Array Semi Dynamic.cpp
@@ -1,32 +1,54 @@
-// Add Elements To Array Semi Dynamic
-// https://programmingadvices.com/courses/1811531/lectures/41413474/comments/25786739
+/*
+    Problem: Add Elements To Array Semi Dynamic
+    Coded By: @X99099
+*/
 
 #include <iostream>
-
 using namespace std;
 
-bool AddElements(int arr[], int& length)
-{
-	bool more;
-	cout << "\nEnter A Number: ";
-	cin >> arr[length++];
-	cout << "Do You Want To Add More? [0]:No [1]:Yes? ";
-	cin >> more;
-	if (!more) return false;
+// Function to get a number from the user
+int getNumber(const string& prompt) {
+    int number;
+    cout << prompt;
+    cin >> number;
+    return number;
 }
 
-void PrintArray(int arr[], int length)
-{
-	for (int i = 0; i < length; i++)
-		cout << arr[i] << ' ';
-	cout << endl;
+// Function to add a number to the array and track its position
+void addNumberToArray(int arr[100], int& size, int number) {
+    arr[size] = number;
+    size++;
 }
 
-int main()
-{
-	int arr[100], length = 0;
-	while (AddElements(arr, length));
-	cout << "\nArray Length  : " << length << endl;
-	cout << "Array Elements: ";
-	PrintArray(arr, length);
+// Function to handle user input for adding numbers to the array
+void inputUserNumbersInArray(int arr[100], int& size) {
+    bool add = true;
+    do {
+        addNumberToArray(arr, size, getNumber("Please Enter a Number: "));
+        add = getNumber("Do You Want To Add More Numbers [1]YES [0]NO: ");
+    } while (add);
+}
+
+// Function to print the contents of an array
+void printArray(const int array[], int size) {
+    cout << "Array Elements: ";
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+    cout << endl;
+}
+
+// Main function: Program entry point
+int main() {
+    int arr[100];      
+    int arrLength = 0; 
+
+    // Prompt the user to input numbers into the array
+    inputUserNumbersInArray(arr, arrLength);
+
+    // Print the final array length and its elements
+    cout << "\nArray Length: " << arrLength << endl;
+    printArray(arr, arrLength);
+
+    return 0; // Return 0 to indicate successful program execution
 }
