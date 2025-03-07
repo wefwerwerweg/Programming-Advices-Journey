461008a1514090cbefaee444a9aeeed16bb21d0a
wefwerwerweg <162762660+wefwerwerweg@users.noreply.github.com>
2025-03-07 08:41:21 +0300

    Update #07 Reverse A Number.cpp

--- 05 - Algorithms & Problem-Solving Level 2/#07 Reverse A Number.cpp
+++ 05 - Algorithms & Problem-Solving Level 2/#07 Reverse A Number.cpp
@@ -1,22 +1,36 @@
-// Reverse A Number
-// https://programmingadvices.com/courses/1811531/lectures/41327443/comments/25772973
+// Problem: Reverse the Digits of a Positive Number
+// Coded By: @X99099
 
 #include <iostream>
-
 using namespace std;
 
-string Reverse(string n)
+int ReadPositiveNumber(string Message)
 {
-	string reversed;
-	for (int i = n.length() - 1; i >= 0; i--)
-		reversed += n[i];
-	return reversed;
+    int Number;
+    do
+    {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number <= 0);
+    return Number;
+}
+
+int ReverseNumber(int Number)
+{
+    int Remainder, Reversed = 0;
+    while (Number > 0)
+    {
+        Remainder = Number % 10;
+        Number /= 10;
+        Reversed = Reversed * 10 + Remainder;
+    }
+    return Reversed;
 }
 
 int main()
 {
-	string n;
-	cout << "Enter A Number To Reverse: ";
-	cin >> n;
-	cout << "Reverse Is: " << Reverse(n);
+    cout << "\nReverse is:\n" 
+         << ReverseNumber(ReadPositiveNumber("Please enter a positive number?"))
+         << "\n";
+    return 0;
 }
