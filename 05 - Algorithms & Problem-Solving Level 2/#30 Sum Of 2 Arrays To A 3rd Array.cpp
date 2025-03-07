304e3552e2986a899f59bf6ff5dd8382cdeffb44
wefwerwerweg <162762660+wefwerwerweg@users.noreply.github.com>
2025-03-07 09:19:49 +0300

    Update #30 Sum Of 2 Arrays To A 3rd Array.cpp

--- 05 - Algorithms & Problem-Solving Level 2/#30 Sum Of 2 Arrays To A 3rd Array.cpp
+++ 05 - Algorithms & Problem-Solving Level 2/#30 Sum Of 2 Arrays To A 3rd Array.cpp
@@ -1,50 +1,62 @@
-// Sum Of 2 Arrays To A 3rd Array
-// https://programmingadvices.com/courses/1811531/lectures/41372962/comments/25782676
+// Problem: Sum of Two Arrays
+// Coded By: @X99099
 
 #include <iostream>
-
 using namespace std;
 
-int RandomNumber(int From, int To)
-{
-	return rand() % (To - From + 1) + From;
+int generateRandom(int from, int to) {
+    return rand() % (to - from + 1) + from;
 }
 
-void AddElements(int arr[], int length)
-{
-	for (int i = 0; i < length; i++)
-		arr[i] = RandomNumber(1, 100);
+int getPositiveNumber(string prompt) {
+    int number;
+    do {
+        cout << prompt;
+        cin >> number;
+    } while (number <= 0);
+    
+    return number;
 }
 
-void SumArrays(int arr1[], int arr2[], int arrSum[], int length)
-{
-	for (int i = 0; i < length; i++)
-		arrSum[i] = arr1[i] + arr2[i];
+void fillRandomNumbers(int arr[], int length) {
+    for (int i = 0; i < length; i++) {
+        arr[i] = generateRandom(1, 100);
+    }
 }
 
-void PrintArray(int arr[], int length)
-{
-	for (int i = 0; i < length; i++)
-		cout << arr[i] << ' ';
-	cout << endl;
+void addArrays(int arr1[], int arr2[], int result[], int length) {
+    for (int i = 0; i < length; i++) {
+        result[i] = arr1[i] + arr2[i];
+    }
 }
 
-int main()
-{
-	srand((unsigned)time(NULL));
-	int arr1[100], arr2[100], arrSum[100], length;
+void printArray(int arr[], int length) {
+    for (int i = 0; i < length; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
 
-	cout << "Array Length: ";
-	cin >> length;
+int main() {
+    srand((unsigned)time(NULL));
 
-	AddElements(arr1, length);
-	AddElements(arr2, length);
-	SumArrays(arr1, arr2, arrSum, length);
+    int arr[100], arr2[100], arrSum[100];
+    
+    int length = getPositiveNumber("Enter number of elements: ");
 
-	cout << "\nArray 1 Elements:\n";
-	PrintArray(arr1, length);
-	cout << "\nArray 2 Elements:\n";
-	PrintArray(arr2, length);
-	cout << "\nSum Of Array 1 And 2:\n";
-	PrintArray(arrSum, length);
+    fillRandomNumbers(arr, length);
+    fillRandomNumbers(arr2, length);
+
+    addArrays(arr, arr2, arrSum, length);
+
+    cout << "\nArray 1:\n";
+    printArray(arr, length);
+
+    cout << "\nArray 2:\n";
+    printArray(arr2, length);
+
+    cout << "\nSum of arrays:\n";
+    printArray(arrSum, length);
+
+    return 0;
 }
