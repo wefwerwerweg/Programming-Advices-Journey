633bb09a4f640b64b73208ce1ca8fda33beab43a
wefwerwerweg <162762660+wefwerwerweg@users.noreply.github.com>
2025-03-07 08:52:54 +0300

    Update #16 Print All Words From AAA To ZZZ.cpp

--- 05 - Algorithms & Problem-Solving Level 2/#16 Print All Words From AAA To ZZZ.cpp
+++ 05 - Algorithms & Problem-Solving Level 2/#16 Print All Words From AAA To ZZZ.cpp
@@ -1,14 +1,29 @@
-// Print All Words From AAA To ZZZ
-// https://programmingadvices.com/courses/1811531/lectures/41359121/comments/25775378
+// Problem: Print All From AAA to ZZZ
+// Coded By: @X99099
 
 #include <iostream>
-
 using namespace std;
+
+void printAllCombinations()
+{
+    string word = "";
+
+    for (char i = 'A'; i <= 'Z'; i++)
+    {
+        for (char j = 'A'; j <= 'Z'; j++)
+        {
+            for (char k = 'A'; k <= 'Z'; k++)
+            {
+                word = char(i) + char(j) + char(k);
+                cout << word << endl;
+            }
+        }
+        cout << "\n____________________________\n";
+    }
+}
 
 int main()
 {
-	for (int i = 65; i <= 90; i++)
-		for (int j = 65; j <= 90; j++)
-			for (int k = 65; k <= 90; k++)
-				cout << char(i) << char(j) << char(k) << endl;
+    printAllCombinations();
+    return 0;
 }
