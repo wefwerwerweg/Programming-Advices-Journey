c1e7e006bcf68b476357274fd82c32e2a4784406
wefwerwerweg <162762660+wefwerwerweg@users.noreply.github.com>
2025-03-07 08:42:14 +0300

    Update #08 Check A Digit_s Frequency In A Number.cpp

--- 05 - Algorithms & Problem-Solving Level 2/#08 Check A Digit_s Frequency In A Number.cpp
+++ 05 - Algorithms & Problem-Solving Level 2/#08 Check A Digit_s Frequency In A Number.cpp
@@ -1,28 +1,40 @@
-// Check A Digit's Frequency In A Number
-// https://programmingadvices.com/courses/1811531/lectures/41327479/comments/25773060
+// Problem: Count the Frequency of a Digit in a Number
+// Coded By: @X99099
 
 #include <iostream>
-
 using namespace std;
 
-int CheckFrequency(int number, int digit)
+int ReadPositiveNumber(string Message)
 {
-	int frequency = 0;
-	while (number != 0)
-	{
-		if (number % 10 == digit) frequency++;
-		number /= 10;
-	}
-	return frequency;
+    int Number;
+    do
+    {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number <= 0);
+    return Number;
+}
+
+int CountDigitFrequency(short DigitToCheck, int Number)
+{
+    int Count = 0, Remainder;
+    while (Number > 0)
+    {
+        Remainder = Number % 10;
+        Number /= 10;
+        if (DigitToCheck == Remainder)
+            Count++;
+    }
+    return Count;
 }
 
 int main()
 {
-	int number, digit;
-	cout << "Enter A Number: ";
-	cin >> number;
-	cout << "Enter A Digit : ";
-	cin >> digit;
-
-	cout << "\nDigit " << digit << " Frequency Is " << CheckFrequency(number, digit) << " Time(s).\n";
+    int Number = ReadPositiveNumber("Please enter the main number?");
+    short DigitToCheck = ReadPositiveNumber("Please enter one digit to check?");
+    
+    cout << "\nDigit " << DigitToCheck << " Frequency is "
+         << CountDigitFrequency(DigitToCheck, Number) << " Time(s).\n";
+    
+    return 0;
 }
