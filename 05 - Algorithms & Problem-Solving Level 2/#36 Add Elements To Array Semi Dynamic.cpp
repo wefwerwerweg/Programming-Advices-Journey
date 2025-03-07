19f56005e47ad89a93eb977d44b7f298d6088af7
wefwerwerweg <162762660+wefwerwerweg@users.noreply.github.com>
2025-03-07 09:30:13 +0300

    Update #36 Add Elements To Array Semi Dynamic.cpp

--- 05 - Algorithms & Problem-Solving Level 2/#36 Add Elements To Array Semi Dynamic.cpp
+++ 05 - Algorithms & Problem-Solving Level 2/#36 Add Elements To Array Semi Dynamic.cpp
@@ -1,53 +1,43 @@
-/*
-    Problem: Add Elements To Array Semi Dynamic
-    Coded By: @X99099
-*/
+// Problem: Add Elements To Array Semi Dynamic
+// Coded By: @X99099
 
 #include <iostream>
 using namespace std;
 
-// Function to get a number from the user
 int getNumber(const string& prompt) {
-    int number;
+    int num;
     cout << prompt;
-    cin >> number;
-    return number;
+    cin >> num;
+    return num;
 }
 
-// Function to add a number to the array and track its position
-void addNumberToArray(int arr[100], int& size, int number) {
-    arr[size] = number;
-    size++;
+void addToArr(int arr[100], int& size, int num) {
+    arr[size++] = num;
 }
 
-// Function to handle user input for adding numbers to the array
-void inputUserNumbersInArray(int arr[100], int& size) {
+void inputNumbers(int arr[100], int& size) {
     bool add = true;
     do {
-        addNumberToArray(arr, size, getNumber("Please Enter a Number: "));
+        addToArr(arr, size, getNumber("Please Enter a Number: "));
         add = getNumber("Do You Want To Add More Numbers [1]YES [0]NO: ");
     } while (add);
 }
 
-// Function to print the contents of an array
-void printArray(const int array[], int size) {
-    cout << "Array Elements: ";
+void printArr(const int arr[], int size) {
     for (int i = 0; i < size; i++) {
-        cout << array[i] << " ";
+        cout << arr[i] << " ";
     }
     cout << endl;
 }
 
-// Main function: Program entry point
 int main() {
-    int arr[100];      
-    int arrLength = 0; 
+    int arr[100];
+    int arrLength = 0;
 
-    // Prompt the user to input numbers into the array
-    inputUserNumbersInArray(arr, arrLength);
+    inputNumbers(arr, arrLength);
 
-    // Print the final array length and its elements
     cout << "\nArray Length: " << arrLength << endl;
-    printArray(arr, arrLength);
+    printArr(arr, arrLength);
 
-    return 0; 
+    return 0;
+}
