dfed93cc1531f199ac788e3c250767b7c9bfdb9f
wefwerwerweg <162762660+wefwerwerweg@users.noreply.github.com>
2025-03-07 09:12:36 +0300

    Update #27 Avg Of Random Array.cpp

--- 05 - Algorithms & Problem-Solving Level 2/#27 Avg Of Random Array.cpp
+++ 05 - Algorithms & Problem-Solving Level 2/#27 Avg Of Random Array.cpp
@@ -1,31 +1,56 @@
 // Average Of Random Array
-// https://programmingadvices.com/courses/1811531/lectures/41361276/comments/25782409
+// Coded By: @X99099
 
 #include <iostream>
-
 using namespace std;
 
-int RandomNumber(int From, int To)
+int RandomNumber(int from, int to)
 {
-	int randNum = rand() % (To - From + 1) + From;
-	return randNum;
+    return rand() % (to - from + 1) + from;
 }
 
-void AddElements(int arr[], int length)
+void FillArray(int arr[100], int &length)
 {
-	for (int i = 0; i < length; i++)
-		arr[i] = RandomNumber(1, 100);
+    cin >> length;
+    for (int i = 0; i < length; i++)
+        arr[i] = RandomNumber(1, 100);
 }
 
-void PrintArray(int arr[], int length)
+void PrintArray(int arr[100], int length)
 {
-	cout << "\nArray Elements: ";
-	for (int i = 0; i < length; i++)
-		cout << arr[i] << ' ';
-	cout << endl;
+    for (int i = 0; i < length; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
 }
 
-float Average(int arr[], int length)
+int SumArray(int arr[100], int length)
+{
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+        sum += arr[i];
+    return sum;
+}
+
+float Average(int arr[100], int length)
+{
+    return (float)SumArray(arr, length) / length;
+}
+
+int main() {
+    srand((unsigned)time(0));
+    
+    int arr[100], length;
+    FillArray(arr, length);
+    
+    if (length <= 0 || length > 100)
+        return 0;
+
+    PrintArray(arr, length);
+    
+    cout << "\nAverage: " << Average(arr, length) << endl;
+
+    return 0;
+}h)
 {
 	int sum = 0;
 	for (int i = 0; i < length; i++)
