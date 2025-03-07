28f35f7ca576202b27af5cbd8b81beaa91d96c91
wefwerwerweg <162762660+wefwerwerweg@users.noreply.github.com>
2025-03-07 08:55:35 +0300

    Update #17 Guess a 3-Letter Password.cpp

--- 05 - Algorithms & Problem-Solving Level 2/#17 Guess a 3-Letter Password.cpp
+++ 05 - Algorithms & Problem-Solving Level 2/#17 Guess a 3-Letter Password.cpp
@@ -1,34 +1,49 @@
-// Guess a 3-Letter Password
-// https://programmingadvices.com/courses/1811531/lectures/41359449/comments/25775763
+// Problem: Guess a 3-Letter Password by Brute Force
+// Coded By: @X99099
 
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-void GuessPassword(string pass)
+string ReadPassword()
 {
-	int trials = 0;
+    string password;
+    cout << "Please enter a 3-Letter Password (all capital)?\n";
+    cin >> password;
+    return password;
+}
 
-	for (char i = 'A'; i <= 'Z'; i++)
-		for (char j = 'A'; j <= 'Z'; j++)
-			for (char k = 'A'; k <= 'Z'; k++)
-			{
-				cout << "Trial [" << ++trials << "]: " << i << j << k << endl;
+bool GuessPassword(string originalPassword)
+{
+    string word;
+    int counter = 0;
+    
+    for (char i = 'A'; i <= 'Z'; i++) 
+    {
+        for (char j = 'A'; j <= 'Z'; j++) 
+        {
+            for (char k = 'A'; k <= 'Z'; k++) 
+            {
+                word = {i, j, k};  // Directly build the string with characters
+                counter++;
 
-				if (pass[0] == i && pass[1] == j && pass[2] == k)
-				{
-					cout << "\nPassword Is " << i << j << k;
-					cout << "\nFound After " << trials << " Trial(s)" << endl;
-					return;
-				}
-			}
+                cout << "Trial [" << counter << "] : " << word << endl;
+
+                if (word == originalPassword)
+                {
+                    cout << "\nPassword is " << word << "\n";
+                    cout << "Found after " << counter << " Trial(s)\n";
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
 }
 
 int main()
 {
-	string pass;
-	cout << "Enter a 3-Letter Uppercase Password: ";
-	cin >> pass;
-	cout << endl;
-	GuessPassword(pass);
+    GuessPassword(ReadPassword());
+    return 0;
 }
