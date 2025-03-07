de1445580e6690ac32275dbda48fa8e0beaab2e6
wefwerwerweg <162762660+wefwerwerweg@users.noreply.github.com>
2025-03-07 08:38:34 +0300

    Update #04 Print All Perfect Numbers From 1 To N.cpp

--- 05 - Algorithms & Problem-Solving Level 2/#04 Print All Perfect Numbers From 1 To N.cpp
+++ 05 - Algorithms & Problem-Solving Level 2/#04 Print All Perfect Numbers From 1 To N.cpp
@@ -1,26 +1,46 @@
-// Print All Perfect Numbers From 1 To N
-// https://programmingadvices.com/courses/1811531/lectures/41206435/comments/25772387
+// Problem: Print Perfect Numbers Up to N
+// Coded By: @X99099
 
 #include <iostream>
-
+#include <string>
 using namespace std;
 
-bool CheckPerfectNumber(int n)
+int GetPositiveNumber(string Message)
 {
-	int sum = 0;
+    int Number = 0;
+    do
+    {
+        cout << Message << endl;
+        cin >> Number;
+    } while (Number <= 0);
+    
+    return Number;
+}
 
-	for (int i = 1; i <= n / 2; i++)
-		if (n % i == 0) sum += i;
+bool IsPerfectNumber(int Number)
+{
+    int Sum = 0;
+    
+    for (int i = 1; i < Number; i++)
+    {
+        if (Number % i == 0)
+            Sum += i;
+    }
+    
+    return Number == Sum;
+}
 
-	return sum == n;
+void DisplayPerfectNumbers(int Number)
+{
+    for (int i = 1; i <= Number; i++)
+    {
+        if (IsPerfectNumber(i))
+            cout << i << endl;
+    }
 }
 
 int main()
 {
-	int n;
-	cout << "Print All Perfect Numbers From 1 To: ";
-	cin >> n;
-	
-	for (int i = 1; i <= n; i++)
-		if (CheckPerfectNumber(i)) cout << i << endl;
+    DisplayPerfectNumbers(GetPositiveNumber("Enter a positive number: "));
+    return 0;
 }
