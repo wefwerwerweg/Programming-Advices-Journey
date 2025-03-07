036139b351118a5c59fb61cd6cf77c6c4e812d17
wefwerwerweg <162762660+wefwerwerweg@users.noreply.github.com>
2025-03-07 09:16:18 +0300

    Update #29 Copy Only Prime Numbers.cpp

--- 05 - Algorithms & Problem-Solving Level 2/#29 Copy Only Prime Numbers.cpp
+++ 05 - Algorithms & Problem-Solving Level 2/#29 Copy Only Prime Numbers.cpp
@@ -2,81 +2,59 @@
 // Coded By: @X99099
 
 #include <iostream>
-#include <cstdlib>
-#include <ctime>
-#include <cmath>
-
 using namespace std;
 
-// Function to generate a random number in a given range
-int randomNumber(int from, int to) {
+enum enPrimNotPrime { Prime = 1, NotPrime = 2 };
+
+enPrimNotPrime CheckPrime(int num) {
+    if (num < 2) return enPrimNotPrime::NotPrime;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) return enPrimNotPrime::NotPrime;
+    }
+    return enPrimNotPrime::Prime;
+}
+
+int RandomNumber(int from, int to) {
     return rand() % (to - from + 1) + from;
 }
 
-// Function to print the contents of an array
-void printArray(const int array[], int arrLength) {
-    for (int i = 0; i < arrLength; i++) {
-        cout << array[i] << " ";
+void FillArray(int arr[100], int &size) {
+    cout << "Enter number of elements: ";
+    cin >> size;
+    for (int i = 0; i < size; i++) 
+        arr[i] = RandomNumber(1, 100);
+}
+
+void CopyPrimes(int src[100], int dest[100], int size, int &destSize) {
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        if (CheckPrime(src[i]) == enPrimNotPrime::Prime) {
+            dest[count++] = src[i];
+        }
     }
+    destSize = count;
+}
+
+void PrintArray(int arr[100], int size) {
+    for (int i = 0; i < size; i++) 
+        cout << arr[i] << " ";
     cout << endl;
 }
 
-// Function to fill an array with random numbers in a given range
-void fillArray(int array[], int arrLength, int from = 1, int to = 99) {
-    for (int i = 0; i < arrLength; i++) {
-        array[i] = randomNumber(from, to);
-    }
-}
-
-// Function to add a number to the array and update its length
-void addArrayElement(int arr[], int& arrLength, int number) {
-    arr[arrLength++] = number;
-}
-
-// Function to check if a number is prime
-bool isPrime(int x) {
-    if (x < 2) return false;
-    if (x == 2 || x == 3) return true;
-    if (x % 2 == 0 || x % 3 == 0) return false;
-
-    int sqrtX = sqrt(x);
-    for (int i = 5; i <= sqrtX; i += 6) {
-        if (x % i == 0 || x % (i + 2) == 0)
-            return false;
-    }
-
-    return true;
-}
-
-// Function to copy only prime numbers from one array to another
-void copyPrimeNumbers(const int source[], int destination[], int sourceLength, int& destinationLength) {
-    for (int i = 0; i < sourceLength; i++) {
-        if (isPrime(source[i])) {
-            addArrayElement(destination, destinationLength, source[i]);
-        }
-    }
-}
-
-// Main function: Program entry point
 int main() {
     srand((unsigned)time(NULL));
 
-    const int MAX_SIZE = 100;
-    int arr[MAX_SIZE];
-    int arrLength = 10;
+    int arr[100], arrLength;
+    FillArray(arr, arrLength);
 
-    fillArray(arr, arrLength);
+    int arr2[100], arr2Length;
+    CopyPrimes(arr, arr2, arrLength, arr2Length);
 
-    int primeArr[MAX_SIZE];
-    int primeArrLength = 0;
+    cout << "\nArray 1 (random numbers): ";
+    PrintArray(arr, arrLength);
 
-    copyPrimeNumbers(arr, primeArr, arrLength, primeArrLength);
-
-    cout << "\nOriginal Array Elements:\n";
-    printArray(arr, arrLength);
-
-    cout << "\nPrime Numbers Array:\n";
-    printArray(primeArr, primeArrLength);
+    cout << "\nArray 2 (prime numbers): ";
+    PrintArray(arr2, arr2Length);
 
     return 0;
 }
