76e83009391fe300d29b63410faa58c17acda991
wefwerwerweg <162762660+wefwerwerweg@users.noreply.github.com>
2025-03-07 09:15:16 +0300

    Update #28 Copy Array.cpp

--- 05 - Algorithms & Problem-Solving Level 2/#28 Copy Array.cpp
+++ 05 - Algorithms & Problem-Solving Level 2/#28 Copy Array.cpp
@@ -1,46 +1,50 @@
 // Copy Array
-// https://programmingadvices.com/courses/1811531/lectures/41372866/comments/25782471
+// Coded By: @X99099
 
 #include <iostream>
-
 using namespace std;
 
-int RandomNumber(int From, int To)
-{
-	return rand() % (To - From + 1) + From;
+const int MAX_SIZE = 100;
+
+int RandomNumber(int From, int To) {
+    return rand() % (To - From + 1) + From;
 }
 
-void AddElements(int arr[], int length)
-{
-	for (int i = 0; i < length; i++)
-		arr[i] = RandomNumber(1, 100);
+void FillArray(int arr[MAX_SIZE], int &size) {
+    cout << "Enter number of elements (1 to " << MAX_SIZE << "): ";
+    cin >> size;
+    if (size < 1 || size > MAX_SIZE) return;
+    for (int i = 0; i < size; i++) 
+        arr[i] = RandomNumber(1, 100);
 }
 
-void PrintArray(int arr[], int length)
-{
-	for (int i = 0; i < length; i++)
-		cout << arr[i] << ' ';
-	cout << endl;
+void PrintArray(int arr[MAX_SIZE], int size) {
+    for (int i = 0; i < size; i++) 
+        cout << arr[i] << " ";
+    cout << endl;
 }
 
-void CopyArray(int arr1[], int arr2[], int length)
-{
-	for (int i = 0; i < length; i++)
-		arr2[i] = arr1[i];
+void CopyArray(int src[MAX_SIZE], int dest[MAX_SIZE], int size) {
+    for (int i = 0; i < size; i++) 
+        dest[i] = src[i];
 }
 
-int main()
-{
-	srand((unsigned)time(NULL));
-	int arr1[100], arr2[100], length;
-	cout << "Array Length: ";
-	cin >> length;
+int main() {
+    srand((unsigned)time(NULL));
+    
+    int arr[MAX_SIZE], arrLength;
+    FillArray(arr, arrLength);
+    
+    if (arrLength <= 0 || arrLength > MAX_SIZE) return 0;
 
-	AddElements(arr1, length);
-	CopyArray(arr1, arr2, length);
+    int arrCopy[MAX_SIZE];
+    CopyArray(arr, arrCopy, arrLength);
 
-	cout << "\nArray 1 Elements:\n";
-	PrintArray(arr1, length);
-	cout << "\nArray 2 Elements After Copy:\n";
-	PrintArray(arr2, length);
+    cout << "\nArray 1 elements: ";
+    PrintArray(arr, arrLength);
+    
+    cout << "\nArray 2 elements after copy: ";
+    PrintArray(arrCopy, arrLength);
+
+    return 0;
 }
