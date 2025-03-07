306c7ea1336652e1f86e1c8e10fbdc6909eb4748
wefwerwerweg <162762660+wefwerwerweg@users.noreply.github.com>
2025-03-07 09:05:27 +0300

    Update #24 Max Of Random Array.cpp

--- 05 - Algorithms & Problem-Solving Level 2/#24 Max Of Random Array.cpp
+++ 05 - Algorithms & Problem-Solving Level 2/#24 Max Of Random Array.cpp
@@ -1,45 +1,57 @@
 // Max Of Random Array
-// https://programmingadvices.com/courses/1811531/lectures/41360211/comments/25782347
+// Coded By: @X99099
 
 #include <iostream>
-
 using namespace std;
 
-int RandomNumber(int From, int To)
+int RandomNumber(int from, int to)
 {
-	int randNum = rand() % (To - From + 1) + From;
-	return randNum;
+    return rand() % (to - from + 1) + from;
 }
 
-void AddElements(int arr[], int length)
+void FillArrayWithRandomNumbers(int arr[100], int &arrLength)
 {
-	for (int i = 0; i < length; i++)
-		arr[i] = RandomNumber(1, 100);
+    cout << "\nEnter number of elements:\n";
+    cin >> arrLength;
+    
+    for (int i = 0; i < arrLength; i++)
+        arr[i] = RandomNumber(1, 100);
 }
 
-void PrintArray(int arr[], int length)
+void PrintArray(int arr[100], int arrLength)
 {
-	cout << "\nArray Elements: ";
-	for (int i = 0; i < length; i++)
-		cout << arr[i] << ' ';
-	cout << endl;
+    for (int i = 0; i < arrLength; i++)
+        cout << arr[i] << " ";
+    cout << "\n";
 }
 
-int MaxNumber(int arr[], int length)
+int MaxNumberInArray(int arr[100], int arrLength)
 {
-	int max = 0;
-	for (int i = 0; i < length; i++)
-		if (arr[i] > max) max = arr[i];
-	return max;
+    int max = 0;
+    
+    for (int i = 0; i < arrLength; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
 }
 
 int main()
 {
-	srand((unsigned)time(NULL));
-	int arr[100], length;
-	cout << "Array Length: ";
-	cin >> length;
-	AddElements(arr, length);
-	PrintArray(arr, length);
-	cout << "Max Number Is: " << MaxNumber(arr, length) << endl;
+    srand((unsigned)time(NULL));
+
+    int arr[100], arrLength;
+
+    FillArrayWithRandomNumbers(arr, arrLength);
+
+    cout << "\nArray Elements: ";
+    PrintArray(arr, arrLength);
+
+    cout << "\nMax Number is : ";
+    cout << MaxNumberInArray(arr, arrLength) << endl;
+
+    return 0;
 }
