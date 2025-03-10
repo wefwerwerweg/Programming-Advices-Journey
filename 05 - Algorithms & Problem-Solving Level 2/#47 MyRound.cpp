1ed78371f8db0ae6a537552d5f631845557a7c0b
wefwerwerweg <162762660+wefwerwerweg@users.noreply.github.com>
2025-03-11 00:22:54 +0300

    Update #47 MyRound.cpp

--- 05 - Algorithms & Problem-Solving Level 2/#47 MyRound.cpp
+++ 05 - Algorithms & Problem-Solving Level 2/#47 MyRound.cpp
@@ -1,30 +1,45 @@
-// MyRound
-// https://programmingadvices.com/courses/1811531/lectures/41457372/comments/25788323
+// Problem: MyRound Function
+// Coded By: X99099
 
 #include <iostream>
-
+#include <cmath>  
 using namespace std;
 
-int MyRound(float num)
-{
-	int numToInt = int(num);
-	if (num >= 0)
-	{
-		if (num + 0.5 >= numToInt + 1) return numToInt + 1;
-		else return numToInt;
-	}
-	else
-	{
-		if (num - 0.5 <= numToInt - 1) return numToInt - 1;
-		else return numToInt;
-	}
+float getNumber(string message) {
+    float number = 0;
+
+    cout << message;
+    cin >> number;
+
+    return number;
 }
 
-int main()
-{
-	float n;
-	cout << "Enter A Number: ";
-	cin >> n;
-	cout << "\nMyRound Result  : " << MyRound(n);
-	cout << "\nC++ Round Result: " << round(n) << endl;
+int getDecimalPart(float number) {
+    return (number - (int)number) * 10;
+}
+
+int myRound(float number) {
+    float decimalPart = getDecimalPart(number);
+    int inPart = (int)number;
+
+    if (abs(decimalPart) >= 0.5) {
+        if (number > 0) {
+            inPart++;
+        }
+        else {
+            inPart--;
+        }
+    }
+
+    return inPart;
+}
+
+int main() {
+    float number = getNumber("Please Enter a Number: ");
+    cout << "\n";
+
+    cout << "My Round Result : " << myRound(number) << endl;
+    cout << "C++ Round Result: " << round(number) << endl; 
+
+    return 0;
 }
