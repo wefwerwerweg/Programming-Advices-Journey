65e587617ab5407c2f93a83b78b5035dd353aa10
wefwerwerweg <162762660+wefwerwerweg@users.noreply.github.com>
2025-03-10 22:40:59 +0300

    Update #45 Count Negative Numbers In Array.cpp

--- 05 - Algorithms & Problem-Solving Level 2/#45 Count Negative Numbers In Array.cpp
+++ 05 - Algorithms & Problem-Solving Level 2/#45 Count Negative Numbers In Array.cpp
@@ -1,44 +1,60 @@
-// Count Negative Numbers In Array
-// https://programmingadvices.com/courses/1811531/lectures/41431575/comments/25787942
+// Problem: Count Negative Numbers In Array
+// Coded By: @X99099 
 
 #include <iostream>
-
 using namespace std;
 
-int RandomNumber(int From, int To)
-{
-	return rand() % (To - From + 1) + From;
+int generateNumber(int min, int max) {
+    return rand() % (max - min + 1) + min;
 }
 
-void AddElements(int arr[], int length)
+int getNumber(string message)
 {
-	for (int i = 0; i < length; i++)
-		arr[i] = RandomNumber(-100, 100);
+    int number = 0;
+
+    cout << message;
+    cin >> number;
+
+    return number;
 }
 
-int CountNegativeNumsInArray(int arr[], int length)
+void fillArray(int arr[], int length)
 {
-	int count = 0;
-	for (int i = 0; i < length; i++)
-		if (arr[i] < 0) count++;
-	return count;
+    for (int i = 0; i < length; i++) {
+        arr[i] = generateNumber(-100, 100);
+    }
 }
 
-void PrintArray(int arr[], int length)
+void printArray(int arr[], int length)
 {
-	for (int i = 0; i < length; i++)
-		cout << arr[i] << ' ';
-	cout << endl;
+    for (int i = 0; i < length; i++) {
+
+        cout << arr[i] << " ";
+
+    }
+
+    cout << endl;
 }
 
-int main()
-{
-	srand((unsigned)time(NULL));
-	int arr[100], length;
-	cout << "Number Of Elements: ";
-	cin >> length;
-	AddElements(arr, length);
-	cout << "\nArray Elements:";
-	PrintArray(arr, length);
-	cout << "\nNegative Numbers Count In Array: " << CountNegativeNumsInArray(arr, length) << endl;
+int countNegativeNumbersInArray(const int arr[], int length) {
+    int count = 0;
+    for (int i = 0; i < length; i++) {
+        if (arr[i] <= 0) count++;
+    }
+    return count;
+}
+
+int main() {
+    srand((unsigned)time(NULL));
+
+    int arr[100];
+    int length = getNumber("Please Enter Array Length: ");    
+    cout << "\n";
+
+    fillArray(arr, length);
+    printArray(arr, length);
+
+    cout << "Negative Numbers In The Array Is " << countNegativeNumbersInArray(arr, length) << endl;
+
+    return 0;
 }
